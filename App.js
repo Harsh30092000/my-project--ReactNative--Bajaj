03bf828bdb4dfdf69f08a95bac0d501a0bf13c8a
Harsh30092000 <harshith30092000@gmail.com>
2022-01-25 14:37:16 +0530

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Harshith");
+  const [friend, setFriend] = useState({ name: "John", age: "23" });
+  const [people, setPeople] = useState([
+    { name: "Harshith", age: "21" },
+    { name: "John", age: "22" },
+    { name: "Harshith", age: "23" },
+    { name: "John", age: "24" },
+  ]);
+
+  const changeName = () => {
+    if (name === "Harshith") {
+      setName("Unknown");
+    } else {
+      setName("Harshith");
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <Text>My friend's name is {friend.name}</Text>
+      <Text>My friend's age is {friend.age}</Text>
+      <TextInput
+        onChangeText={(val) => setName(val)}
+        style={styles.input}
+        placeholder="Enter your name"
+      />
+      <Button title="Change Name" onPress={changeName} />
+
+      <View style={styles.people}>
+        <ScrollView>
+          {people.map((person, index) => (
+            <Text key={index}>{person.name}</Text>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +52,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffeedd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bigBlue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+  },
+  people: {
+    justifyContent: "space-around",
+    alignItems: "center",
   },
 });
